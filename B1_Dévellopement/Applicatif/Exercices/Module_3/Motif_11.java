5612915f0b44a86f40f460d1c5eb0815c71cff56
Batiste Duroyon <duroyon.babatiste@hotmail.com>
2025-10-10 10:44:29 +0200

    Deleted (not working files) + Edit (working file)

--- B1_Dévellopement/Applicatif/Exercices/Module_3/Motif_11.java
+++ B1_Dévellopement/Applicatif/Exercices/Module_3/Motif_11.java
@@ -2,19 +2,16 @@ package B1_Dévellopement.Applicatif.Exercices.Module_3;
 
 public class Motif_11 {
     public static void main(String[] args) {
-        int width = 32;
-        int height = 9;
-        for (int line = 0; line < height; line++) {
-            for (int pos = 0; pos < width; pos++) {
-                if (pos == line || pos == width - 1 - line) {
-                    System.out.print("/");
-                } else if (pos == width / 2 - 1 || pos == width / 2) {
-                    System.out.print("!");
-                } else {
-                    System.out.print(" ");
-                }
-            }
+        int width = 22;
+        for (int start = 0; start < ((width)/2); start++) {
+            System.out.print("\\".repeat(start));
+            System.out.print("!".repeat(width - (start * 2)));
+            System.out.print("/".repeat(start));
             System.out.println();
+            start++;
+            }
+            
         }
     }
-}
+
+
